5c6f9d2f26bbeb8b2c385abad348d4276264c289
Alin Ciobanu <alin.ciobanu@teamnet.ro>
2014-11-03 22:32:21 +0200

    new carousels

--- client/app/main/carouselDescription.js
+++ client/app/main/carouselDescription.js
@@ -1,31 +1,9 @@
 
-var carouselImagesDescription = {
-    0: {
+var carouselImagesDescription = {};
+
+for (var i = 0; i < TOTAL_CAROUSEL_IMAGES; i++) {
+    carouselImagesDescription[i] = {
         title: '',
         description: ''
-    },
-    1: {
-        title: '',
-        description: ''
-    },
-    2: {
-        title: '',
-        description: ''
-    },
-    3: {
-        title: '',
-        description: ''
-    },
-    4: {
-        title: '',
-        description: ''
-    },
-    5: {
-        title: '',
-        description: ''
-    },
-    6: {
-        title: '',
-        description: ''
-    }
-};
+    };
+}
