46fa625db3a52d61a5c8965a1f5996ed6c3971f0
yozmo <alin.emanuel.ciobanu@gmail.com>
2014-11-22 18:49:22 +0200

    load locale

--- client/app/config/LanguageServices.js
+++ client/app/config/LanguageServices.js
@@ -1,21 +1,25 @@
 
+var languagesListGLOBAL = {
+    english: {
+        code: 'en',
+            name: 'English',
+            imgPath: '/images/flags16/EN.png',
+            localePath: 'lib/angular-i18n/angular-locale_en-us.js'
+    },
+    romanian: {
+        code: 'ro',
+            name: 'Română',
+            imgPath: '/images/flags16/RO.png',
+            localePath: 'lib/angular-i18n/angular-locale_ro-ro.js'
+    }
+};
+
 configModule
 
 .factory('Languages', [
 function () {
 
-    var list = {
-        english: {
-            code: 'en',
-            name: 'English',
-            imgPath: '/images/flags16/EN.png'
-        },
-        romanian: {
-            code: 'ro',
-            name: 'Română',
-            imgPath: '/images/flags16/RO.png'
-        }
-    };
+    var list = languagesListGLOBAL;
 
     var findByCode = function (code) {
         return _.find(list, function (lang) {
@@ -31,8 +35,8 @@ function () {
 ])
 
 .factory('CurrentLanguageFactory', [
-'Languages', '$translate',
-function (Languages, $translate) {
+'Languages',
+function (Languages) {
 
     var thisService = {};
 
@@ -52,9 +56,15 @@ function (Languages, $translate) {
     loadFromStorage();
 
     thisService.setLanguage = function (code) {
-        $translate.use(code);
-        thisService.language = Languages.findByCode(code);
+
+        if (code == thisService.language.code) {
+            return;
+        }
+
+//        $translate.use(code);
+//        thisService.language = Languages.findByCode(code);
         localStorage.setItem('Lang', JSON.stringify({code: code}));
+        location.reload(); // reload is needed to load correct angular locale
     }
 
     return thisService;
