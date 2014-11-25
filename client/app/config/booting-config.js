e487813e05ae38ba0cb6754c8f1dc54f1dff85d0
yozmo <alin.emanuel.ciobanu@gmail.com>
2014-11-25 21:53:36 +0200

    facebook dynamically loading share button

--- client/app/config/booting-config.js
+++ client/app/config/booting-config.js
@@ -1,12 +1,13 @@
 
 configModule.run([
-'$rootScope', 'Templates', 'UserInformation', 'CurrentLanguageFactory', 'Languages',
-function ($rootScope, Templates, UserInformation, CurrentLanguageFactory, Languages) {
+'$rootScope', 'Templates', 'UserInformation', 'CurrentLanguageFactory', 'Languages', 'Settings',
+function ($rootScope, Templates, UserInformation, CurrentLanguageFactory, Languages, Settings) {
 
     $rootScope.Templates = Templates;
     $rootScope.userInfo = UserInformation;
     $rootScope.CurrentLanguageFactory = CurrentLanguageFactory;
     $rootScope.Languages = Languages;
+    $rootScope.Settings = Settings;
 
 }]
 );
